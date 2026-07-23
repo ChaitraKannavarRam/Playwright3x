14210e8651e76ca5da7644d5b0dcf2df9acdbaf3
Chaitra K R <chaitrarkannavar@gmail.com>
2026-07-23 20:04:36 +0530

    Add HackerRank solved problems for if-else and switch.

---
+++ Chapter_04_Switch/HackerRank_Day1_Q.js
@@ -0,0 +1,34 @@
+let data = require('fs').readFileSync(0, 'utf8');
+let num = parseInt(data);
+
+switch (num) {  //day is an integer varying from 0 to 6.
+    case 1:
+        console.log("ONE");
+        break;
+    case 2:
+        console.log("TWO");
+        break;
+    case 3:
+        console.log("THREE");
+        break;
+    case 4:
+        console.log("FOUR");
+        break;
+    case 5:
+        console.log("FIVE");
+        break;
+    case 6:
+        console.log("SIX");
+        break;
+    case 7:
+        console.log("SEVEN");
+        break;
+    case 8:
+        console.log("EIGHT");
+        break;
+    case 9:
+        console.log("NINE");
+        break;
+    default:
+        console.log("PLEASE TRY AGAIN");
+        }
