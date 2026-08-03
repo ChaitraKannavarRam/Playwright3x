187ac32848126584ba35a429b0cea9ce7320e08e
Chaitra K R <chaitrarkannavar@gmail.com>
2026-08-03 19:38:32 +0530

    Add solution to reverse array elements

---
+++ Coding_Practice/Reverse_Array_Elements.js
@@ -0,0 +1,7 @@
+const data = require('fs').readFileSync(0, 'utf8');
+const tokens = data.split(/\s+/).filter(s => s.length > 0).map(Number);
+const n = tokens[0];
+const arr = tokens.slice(1, 1 + n);
+
+// Write your solution here'
+console.log( arr.reverse().join(' '));
