f8cfb5311092a5e355f66688a450cde4d0609a07
Chaitra K R <chaitrarkannavar@gmail.com>
2026-08-28 14:10:59 +0530

    Add async/await examples and error handling patterns in various scripts

--- Chapter_18_Async_Await/147_BetterWay.js
+++ Chapter_18_Async_Await/147_BetterWay.js
@@ -22,41 +22,41 @@ function clickLogin() {
     });
 }
 
-async function runLoginFlow() {
-    let msg1 = await openBrowser();
-    console.log("Step 1:", msg1);
+// async function runLoginFlow() {
+//     let msg1 = await openBrowser();
+//     console.log("Step 1:", msg1);
 
-    let msg2 = await goToLogin();
-    console.log("Step 2:", msg2);
+//     let msg2 = await goToLogin();
+//     console.log("Step 2:", msg2);
 
-    let msg3 = await enterCredentials();
-    console.log("Step 3:", msg3);
+//     let msg3 = await enterCredentials();
+//     console.log("Step 3:", msg3);
 
-    let msg4 = await clickLogin();
-    console.log("Step 4:", msg4);
+//     let msg4 = await clickLogin();
+//     console.log("Step 4:", msg4);
     
-}
+// }
 
 
 
 
-// openBrowser()
-//     .then(function (msg) {
-//         console.log("Step 1", msg);
-//         return goToLogin();
-//     }).then(function (msg) {
-//         console.log("Step 2 :", msg);
-//         return enterCredentials();
-//     }).then(function (msg) {
-//         console.log("Step 3 :", msg);
-//         return clickLogin();
-//     }).then(function (msg) {
-//         console.log("Step 4 :", msg);
-//     }).catch(function (error) {
-//         console.log("Error:", error);
-//     }).finally(function () {
-//         console.log("Done execution!");
-//     });
+openBrowser()
+    .then(function (msg) {
+        console.log("Step 1", msg);
+        return goToLogin();
+    }).then(function (msg) {
+        console.log("Step 2 :", msg);
+        return enterCredentials();
+    }).then(function (msg) {
+        console.log("Step 3 :", msg);
+        return clickLogin();
+    }).then(function (msg) {
+        console.log("Step 4 :", msg);
+    }).catch(function (error) {
+        console.log("Error:", error);
+    }).finally(function () {
+        console.log("Done execution!");
+    });
 
 
 
